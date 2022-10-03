0c8cd36db9f6318f3fa5583d3e28edf87584dbf6
Alexey <a.d.kamenev@yandex.ru>
2022-10-03 12:47:16 +0300

    v2.0.1

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -1,21 +1,56 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Bag {
     // рюкзак должен обладать грузоподъемностью и вмещать вещи, суммарно не превышающие её
     private final double volume;
+    double freeSpace; // После укладки будет оставаться свободное место.
+    double unitedUsefull; // общая полезность вещей в рюкзаке.
+    double readyWeight;
+    double readyUsefull;
 
     public Bag(double volume) {
         this.volume = volume;
+        this.freeSpace = volume;
     }
 
     ArrayList <Item> items = new ArrayList<>();
-    // грубо говоря - ячейки для заполнения вещами
+    // грубо говоря - весь комплект посчитаных вещей
 
-    public void putItemInBag (Item[] item){  // для удобства реализации я планирую работать с массивом вещей
-
+    /**
+     * Метод должен сформировать список всех вещей, подготовленных к упаковке, расчитать их вес и полезность
+     * @param item на вход принимает масси всех вещей
+     */
+    public void readyAllItem (Item[] item){  // для удобства реализации я планирую работать с массивом вещей
         for (int i=0; i< item.length; i++){
             //TODO: заполнить метод добавления вещей в рюкзак
+            readyWeight += item[i].weight; // любая вещь имеет вес
+            readyUsefull += item[i].usefull; // полезность потенциального содержимого рюкзака увеличивается
+            items.add(item[i]); // на этом этапе вещь подготовлена и изменила состояние укладки
         }
+    }
+
+    /**
+     * метод не тестировался
+     * @param items
+     * @return
+     */
+    public ArrayList <Item> completeIn (ArrayList<Item> items) {
+        ArrayList<Item> bagComplete = new ArrayList<>();
+        items.sort(Item::compareTo);
+        int itemCounter = items.size();
+        if (freeSpace == 0 || itemCounter == 0) {
+            return bagComplete;
+        } else if (freeSpace >= items.get(itemCounter).weight) {
+            bagComplete.add(items.get(itemCounter));
+            freeSpace -= items.get(itemCounter).weight;
+            items.remove(itemCounter);
+            return completeIn(items);
+        } else if (freeSpace < items.get(itemCounter).weight) {
+            items.remove(itemCounter);
+            return completeIn(items);
+        }
+        return bagComplete;
     }
 }
--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,7 +1,28 @@
-public class Item {
+import java.util.Objects;
+
+public class Item implements Comparable<Item> {
     // вещь - имеет вес и практическую ценность
     double weight;
     int usefull;
     // будет логично, если вещь будет иметь название (возможно, именно эту программу я смогу применить на практике)
     String name;
+
+    double usability; // пусть вещь имеет полезную нагрузку, по типу КПД, только для вещи
+
+    public double getUsability() {
+        return usability;
+    }
+
+    public Item(double weight, int usefull, String name) {
+        this.weight = weight;
+        this.usefull = usefull;
+        this.name = name;
+        this.usability = (usefull / weight) * 100;
+    }
+
+
+    @Override
+    public int compareTo(Item o) {
+        return (int)(this.getUsability()-o.getUsability());
+    }
 }
